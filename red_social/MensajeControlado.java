07c5209a2ecb8e6d372f3709cbac25976752e33a
claudsze <clasaiz@outlook.es>
2026-03-14 16:54:12 +0100

    documentacion

--- red_social/MensajeControlado.java
+++ red_social/MensajeControlado.java
@@ -1,13 +1,36 @@
+/**
+ *  Este paquete contiene las clases necesarias para la gestión de una red social
+ */
 package red_social;
 
+/**
+ * Esta clase representa un mensaje controlado
+ * @author Claudia Saiz Escribano y Duna Puente Romera. 
+ * @version 1.0
+ * Nombre del fichero: MensajeControlado.java
+ * 
+ */
 public class MensajeControlado extends Mensaje {
 	private int rigidez;
 
+	/**
+	 * Crea un nuevo mensaje controlado
+	 * 
+	 * @param texto, el texto del mensaje
+	 * @param alcance, el alcance del mensaje
+	 * @param usuarioActual, el usuario actual del mensaje
+	 * @param rigidez, la rigidez del mensaje
+	 */
 	public MensajeControlado(String texto, int alcance, Usuario usuarioActual, int rigidez) {
 		super(texto, alcance, usuarioActual);
 		this.rigidez = rigidez;
 	}
 	
+	/**
+	 * Obtiene la rigidez del mensaje
+	 * 
+	 * @return int, el valor de la rigidez
+	 */
 	public int getRigidez() {
 		return rigidez;
 	}
